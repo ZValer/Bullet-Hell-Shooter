3dde3cf0bedcbbad62501de6f880e16a71a448fc
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-11-18 00:09:16 -0600

    Guardar

--- Bullet Hell Shooter/Assets/Scripts/StarShotSettings.cs
+++ Bullet Hell Shooter/Assets/Scripts/StarShotSettings.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [System.Serializable]
 public class StarShotSettings
 {
-    public int NumberOfBullets = 20;
+    public int NumberOfBullets = 50;
     public float BaseBulletSpeed = 10f;
     public float SpeedMultiplier = 1.5f; // Multiplicador de velocidad en los picos
     public float CooldownAfterShot = 2f;
